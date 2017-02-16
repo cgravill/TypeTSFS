64dde189d074f0d275a9e89882d8d420042dd71f
Colin Gravill <colin@gravill.com>
2017-02-16 12:00:57 +0000

    Samples with Json.net

---
+++ TypesTSFSTest/JsonNetSandbox.fsx
@@ -0,0 +1,13 @@
+﻿#r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+
+open Newtonsoft.Json
+
+type Circle = 
+    { Radius : int
+      Numbers : List<int> }
+
+let example = 
+    { Radius = 4
+      Numbers = [ 1; 4; 3 ] }
+
+JsonConvert.SerializeObject(example)
