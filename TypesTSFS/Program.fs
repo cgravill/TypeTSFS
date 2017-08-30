84e45346aab056ec0855cc837700c309237ce28a
Colin Gravill <colin@gravill.com>
2017-08-30 15:20:12 +0100

    Configure from command-line
    It's still possible to directly reference from FSI. However, that extremely tightly binds the usage project's F# version to the library. This became an issue between F# Compiler Services and F# 4.1 in a project using this.

--- TypesTSFS/Program.fs
+++ TypesTSFS/Program.fs
@@ -1,10 +1,33 @@
-﻿[<EntryPoint>]
+﻿open Argu
+
+let moduleNameDefault = "JSAPI"
+
+type CLIArguments =
+    | [<Mandatory>] Style of EmitTS.Style
+    | [<Mandatory>] ProjectFilePath of path:string
+    | ModuleTargetName of moduleName:string
+    | [<Mandatory>] OutputPath of path:string
+
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Style _ -> "style of output, affects TypeScript"
+            | ProjectFilePath _ -> "full path to .fsproj"
+            | ModuleTargetName _ -> sprintf "F# module name, defaults to: %s" moduleNameDefault
+            | OutputPath _ -> "full path, including .ts to output"
+
+[<EntryPoint>]
 let main argv =
+    let parser = ArgumentParser.Create<CLIArguments>(errorHandler=Argu.ProcessExiter())
 
-    let projectFile = @"../../../SampleWS/SampleWS.fsproj"
-    let moduleTargetName = "JSAPI"
-    let outputPath = "output.ts"
+    let arguments = parser.ParseCommandLine(inputs=argv)
 
-    Transformer.fromFSharpViaWebSharperToTypeScript projectFile moduleTargetName outputPath
+    let style = arguments.GetResult(<@ Style @>)
+    let projectFilePath = arguments.GetResult(<@ ProjectFilePath @>)
+    let moduleTargetName = arguments.GetResult(<@ ModuleTargetName @>, defaultValue = moduleNameDefault)
+    let outputPath = arguments.GetResult(<@ OutputPath @>)
+
+    Transformer.fromFSharpToTypeScript style projectFilePath moduleTargetName outputPath
 
     0
